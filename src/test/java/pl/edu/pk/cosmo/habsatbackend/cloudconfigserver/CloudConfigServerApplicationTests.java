8f65e3f0e27f137ef2ffbbebe918ff9e9be0ead8
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2022-11-28 17:30:19 +0100

    initial commit made with :carrot: & :hearts:

---
+++ src/test/java/pl/edu/pk/cosmo/habsatbackend/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pk.cosmo.habsatbackend.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
